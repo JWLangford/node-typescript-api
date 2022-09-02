e71df0d651e1ce647dca1c341bb97928bbd22c28
Jesse Langford <jesse@passkit.com>
2022-09-02 08:51:27 +0800

    continued development helper setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# node-typescript-api
+# Node.js + Typescript API
+
+This repo is the base project for a series of articles showing how to build an API with Node.js and Typescript.
+
+Part 1: [Installation and Setup]
+
+[installation and setup]: https://jesse-94998.medium.com/part-1-node-js-typescript-api-installation-and-setup-b1290285e5a5
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
+import * as dotenv from "dotenv";
 import Server from "./src/server";
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const PORT = parseInt(process.env.PORT || "8080");
 
 const server = Server.listen(PORT, () => {
-  console.log("listening on port %s...", server?.address());
+  console.log(process.env.MESSAGE, server?.address());
 });
